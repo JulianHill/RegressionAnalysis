1fad917b8e454a49e69004c734336bce07eea020
JulianHill <jul.hillebrand@gmail.com>
2015-07-20 20:47:51 +0200

    Fix export function

--- dataPreparation.R
+++ dataPreparation.R
@@ -2,4 +2,10 @@
 #load example dataset
 cars <- mtcars[c(1,2,9,10)]
 cars$gear <- ordered(cars$gear)
-cars$am <- factor(cars$am, labels=c('auto', 'manual'))
+cars$am <- factor(cars$am, labels=c('auto', 'manual' ))
+
+#plot data
+plot(cars)
+
+#perform a correlation test
+cor.test(cars$speed,cars$dist)
